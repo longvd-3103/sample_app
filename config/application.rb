7139345e227dec05eaaf8ba277469c30f6de21f6
Vo Duc Long <vo.duc.long@sun-asterisk.com>
2022-06-10 15:49:50 +0700

    Filling in the layout1

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,14 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :vi
     config.i18n.fallbacks=true
